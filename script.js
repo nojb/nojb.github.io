45b03c9d5fda7e30dcff69c28d9aeb758690e413
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2016-12-09 23:41:20 +0100

    Remove script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function change(name) {
-    var elems = document.getElementsByClassName("section");
-
-    for (var i = 0; i < elems.length; i ++)
-        elems[i].style.display = (elems[i].id == name) ? '' : 'none';
-
-    return false;
-}
